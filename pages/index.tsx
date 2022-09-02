9a8ff925816d8fec961e766935b522b0a97e33b1
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-09-02 08:29:14 +0530

    feat: build tutorial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,50 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  useAddress,
+  useLogin,
+  useUser,
+} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const login = useLogin();
+  const user = useUser();
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch("/api/validate", {
+        method: "POST",
+      });
+
+      const data = await response.json();
+      alert(data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (user.user) {
+    return (
+      <div className={styles.container}>
+        <p>You are signed in as {user.user.address}</p>
+        <button className={styles.btn} onClick={handleClick}>
+          Validate user
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <ConnectWallet accentColor="#f213a4" colorMode="light" />
+    <div className={styles.container}>
+      {address ? (
+        <button className={styles.btn} onClick={() => login()}>
+          Sign in with Ethereum
+        </button>
+      ) : (
+        <ConnectWallet />
+      )}
     </div>
   );
 };
